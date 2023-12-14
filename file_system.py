035b03b33fd892b61e977d93abbc69f8645689e6
Durga Prasad Pradhan <96914435+durga811@users.noreply.github.com>
2023-12-14 20:53:20 +0530

    writing comments in file_system.py

--- file_system.py
+++ file_system.py
@@ -7,7 +7,7 @@ class FileSystem(FileSystemHelper):
     def __init__(self):
         self.root = Directory("root")
         self.current_directory = self.root
-
+#mkdir
     def mkdir(self, dir_name):
         # for cheking for invalid characters in the directory name (i assuming  that '/',"\\" shoud not in directory name it may confuse with paths)
         if '/' in dir_name or '\\' in dir_name:
@@ -16,7 +16,7 @@ class FileSystem(FileSystemHelper):
             return "Directory already exists"
         new_dir = Directory(dir_name, self.current_directory)
         self.current_directory.add_directory(new_dir)
-
+#touch
     def touch(self, file_name):
         # same rule as i applied to directories
         if '/' in file_name or '\\' in file_name:
@@ -25,16 +25,16 @@ class FileSystem(FileSystemHelper):
             return "File already exists"
         new_file = File(file_name)
         self.current_directory.add_file(new_file)
-
+#ls
     def ls(self):
         return self.current_directory.list_contents()
-
+#cat
     def cat(self, file_name):
         for item in self.current_directory.contents:
             if isinstance(item, File) and item.name == file_name:
                 return item.content
         return "File not found"
-
+#echo
     def echo(self, text, file_name, append=False):
         for item in self.current_directory.contents:
             if isinstance(item, File) and item.name == file_name:
@@ -45,7 +45,7 @@ class FileSystem(FileSystemHelper):
                 return
         return "File not found"
 
-
+#rm
     def rm(self, name):
         if name == "root":
             return "Cannot remove root directory"
@@ -54,14 +54,14 @@ class FileSystem(FileSystemHelper):
                 self.current_directory.contents.remove(item)
                 return
         return "File or directory not found"
-
+#mv
     def mv(self, source_path, destination_path):
         # Resolve the full path of the source
         source_full_path = self._resolve_full_path(source_path)
         source_dir_path, _, source_item_name = source_full_path.rpartition('/')
         source_dir = self._find_directory(source_dir_path) if source_dir_path else self.current_directory
 
-        # Find the item to move in the source directory
+        # Finding the item to move
         item_to_move = None
         for item in source_dir.contents:
             if item.name == source_item_name:
@@ -78,17 +78,17 @@ class FileSystem(FileSystemHelper):
         if destination_dir is None:
             return "Destination path not found"
 
-        # Add the item to the new location
+        # moving item to dest location
         destination_dir.contents.append(item_to_move)
 
-
+#cp
     def cp(self, source_path, destination_path):
         # Resolve the full path of the source
         source_full_path = self._resolve_full_path(source_path)
         source_dir_path, _, source_item_name = source_full_path.rpartition('/')
         source_dir = self._find_directory(source_dir_path) if source_dir_path else self.current_directory
 
-        # Find the item to copy in the source directory
+        # finding the item to copy
         item_to_copy = None
         for item in source_dir.contents:
             if item.name == source_item_name:
@@ -104,9 +104,9 @@ class FileSystem(FileSystemHelper):
         if destination_dir is None:
             return "Destination path not found"
 
-        # Add the copied item to the new location
+        # coping to dest location
         destination_dir.contents.append(item_to_copy)
-
+#cd
 
     def cd(self, path):
         if path == ".." and self.current_directory.name == "root":
@@ -118,7 +118,7 @@ class FileSystem(FileSystemHelper):
 
     
     
-    #grep
+ #grep
     def grep(self, pattern):
         found = []
         for item in self.current_directory.contents:
